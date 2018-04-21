017e23b15c9bf1d5f8c72d1b177aa95ae2f15ba0
Piotr Pidlypczak <peterpid@gmail.com>
2018-04-21 19:42:00 +0200

    Add template form for file upload

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 exports.upload = function(request, response) {
     console.log('Request: upload');
     response.write('Upload start!');
@@ -6,8 +8,11 @@ exports.upload = function(request, response) {
 
 exports.welcome = function(request, response) {
     console.log('Request: welcome');
-    response.write('Welcome on start site!');
-    response.end();
+    fs.readFile('templates/start.html', function(err, html) {
+        response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        response.write(html);
+    	response.end();
+	});
 }
 
 exports.error = function(request, response) {
