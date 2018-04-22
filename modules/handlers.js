5dcb7116c343c2ce332bb5658bafda2b125e84ab
Piotr Pidlypczak <peterpid@gmail.com>
2018-04-22 23:34:26 +0200

    Add support for multiple files upload

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,13 +1,56 @@
-var fs = require('fs');
+var fs          = require('fs');
+var formidable  = require('formidable');
+var url         = require('url');
+var querystring = require('querystring');
 
 exports.upload = function(request, response) {
-    console.log('Request: upload');
-    response.write('Upload start!');
-    response.end();
+    var form = new formidable.IncomingForm();
+    form.multiples = true;
+    form.parse(request, function(error, fields, files) {
+    	if (error) {
+    		throw (error);
+    	}
+	    response.writeHead(200, {'Content-Type': 'text/html'});
+	    if (typeof(files) === 'undefined' || typeof(files.upload) === 'undefined') {
+	    	response.end();
+	    	return;
+	    }
+		response.write('received images: <br/>');
+    	for (let fileId = 0; fileId < files.upload.length; fileId++){
+    		if (files.upload[fileId].name === null || files.upload[fileId].name.length === 0) {
+    			break;
+    		}
+	    	var fileDestPath = './files/' + files.upload[fileId].name;
+	        fs.renameSync(files.upload[fileId].path, fileDestPath);
+			console.log('[handlers] upload ', files.upload[fileId].name, ' done');
+    		response.write('<img src="/show?' + files.upload[fileId].name + '"/><br/>');    
+			if (fileId == files.upload.length - 1) {
+				response.end();
+			}
+		}
+	});
+}
+
+exports.show = function(request, response) {
+    //querystring.unescape used to translate eg. %20 to ' '
+    let queryStr = querystring.unescape(url.parse(request.url).query);
+    if (queryStr === 'null') {
+        return;
+    }  
+    let filePath = './files/' + queryStr;
+	console.log('[handlers] file to show: ', filePath);
+    response.writeHead(200, {"Content-Type": "image/png"});
+    fs.readFile(filePath, "binary", function(error, file) {
+    	if (error) {
+    		throw (error);
+    	}
+    	console.log('[handlers] readFile: ', filePath);
+        response.write(file, "binary");
+		response.end();
+    });
 }
 
 exports.welcome = function(request, response) {
-    console.log('Request: welcome');
     fs.readFile('templates/start.html', function(err, html) {
         response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
         response.write(html);
@@ -16,7 +59,7 @@ exports.welcome = function(request, response) {
 }
 
 exports.error = function(request, response) {
-    console.log('Request not recognized');
+    console.log('[handlers] request ', request.url, ' not recognized');
     response.write('404 request not recognized');
     response.end();
 }
--- modules/server.js
+++ modules/server.js
@@ -1,28 +1,29 @@
-var http 	 = require('http');
+var http 	   = require('http');
 var colors   = require('colors');
+var url      = require('url');
 var handlers = require('./handlers');
 
 function start() {
-  function onRequest(request, response) {
-    console.log('Request received'.green);
-    console.log('Request ', request.url , ' received');
-    response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-    switch (request.url) {
-        case '/':
-        case '/start':
-            handlers.welcome(request, response);
-            break;
-        case '/upload':
-            handlers.upload(request, response);
-            break;
-        default:
-            handlers.error(request, response);
+    function onRequest(request, response) {
+        console.log('[server] Request '.green, url.parse(request.url).pathname , ' received'.green);
+        response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+        switch (url.parse(request.url).pathname) {
+            case '/':
+            case '/start':
+                handlers.welcome(request, response);
+                break;
+            case '/upload':
+                handlers.upload(request, response);
+                break;
+            case '/show':
+      	    handlers.show(request, response);
+      	        break;
+            default:
+                handlers.error(request, response);
+        }
     }
-  }
-
-  http.createServer(onRequest).listen(9000);
-
-  console.log('Server running'.green);
+    http.createServer(onRequest).listen(9000);
+    console.log('[server] Server running'.green);
 }
 
 exports.start = start;
